6a92169ecf200fe848a4cba728bbdd5b4e32ad39
bilalTariq00 <bilal002ta@gmail.com>
2024-08-26 09:00:30 -0700

    test

--- src/app/components/HomePage/Layout.jsx
+++ src/app/components/HomePage/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
-        <title>Bilal's Store</title>
+        <title>Ball Store</title>
       </Head>
       <header>
         <Navbar/>
